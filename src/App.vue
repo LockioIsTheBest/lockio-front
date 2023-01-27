c217c6c175cfdeb8290e5611d149a78ddea9be7d
rsaurel <remi.saurel@kosmos.fr>
2023-01-27 08:25:27 +0100

    add: first page + start nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <div>
-    <HelloWorld></HelloWorld>
+  <div class="bg-gray-900 h-screen">
+    <NavBar></NavBar>
+    <div class="text-center">
+      <HelloWorld></HelloWorld>
+    </div>
   </div>
 </template>
 
 <script setup>
 import HelloWorld from "./components/HelloWorld.vue";
+import NavBar from "./components/NavBar.vue";
 </script>
