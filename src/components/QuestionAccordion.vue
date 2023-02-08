0b057b569453c01ace2994d733a95b7bfae46511
Rémi Saurel <remi.saurel@kosmos.fr>
2023-02-08 13:25:38 +0100

    add: misc fix (css, home)

--- src/components/QuestionAccordion.vue
+++ src/components/QuestionAccordion.vue
@@ -1,23 +1,23 @@
 <template>
   <div
-    class="border border-gray-800 rounded-xl bg-white p-3 mt-4"
-    @click="toggle"
-    id="question"
+      class="border border-gray-800 rounded-xl bg-white p-3 mt-4"
+      @click="toggle"
+      id="question"
   >
     <div class="flex justify-between">
       <div class="font-bold pr-4">{{ question }}</div>
       <button v-if="!state.isOpen">
         <img
-          src="/src/assets/arrowdown.svg"
-          alt="arrow"
-          class="h-6 w-6 max-w-none"
+            src="/src/assets/arrowdown.svg"
+            alt="arrow"
+            class="h-6 w-6 max-w-none"
         />
       </button>
       <button v-else>
         <img
-          src="/src/assets/arrowup.svg"
-          alt="arrow"
-          class="h-6 w-6 max-w-none"
+            src="/src/assets/arrowup.svg"
+            alt="arrow"
+            class="h-6 w-6 max-w-none"
         />
       </button>
     </div>
@@ -28,7 +28,7 @@
 </template>
 
 <script setup>
-import { reactive } from "vue";
+import {reactive} from "vue";
 
 const props = defineProps({
   question: {
