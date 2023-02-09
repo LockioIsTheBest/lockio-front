06a225f49262a4f695c49f4a57de8b5a02d45c85
Rémi Saurel <remi.saurel@kosmos.fr>
2023-02-09 23:35:44 +0100

    add: blank profile and reservation page

---
+++ src/layouts/ReservationsPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex flex-row">
+    <div class="hidden md:block">
+      <vertical-navbar></vertical-navbar>
+    </div>
+    <div class="flex flex-col h-screen w-screen ml-8 mr-8 ">
+      <!-- TITLE -->
+      <div class="flex flex-row text-4xl font-bold items-center mt-8">
+        <img src="/src/assets/calendar-dark.svg" alt="logo" class="h-10 w-10 mr-2 mb-2"/>
+        <h1>Reservations</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import VerticalNavbar from "../components/navbars/VerticalNavbar.vue";
+</script>
+
+<style scoped>
+
+</style>
